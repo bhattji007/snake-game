6820a0f0d23a5a44b2a46b9eb9f56757650c80e5
shubham <shubham@saasden.club>
2024-03-23 19:31:46 +0000

    feat: added snake mod

--- src/snake.rs
+++ src/snake.rs
@@ -4,4 +4,27 @@ use piston_window::types::Color;
 
 use crate::draw::draw_block;
 
-const SNAKE_COLOR:Color=[0.00,0.80,0.00,1.0]
+const SNAKE_COLOR:Color=[0.00,0.80,0.00,1.0];
+
+pub enum Direction{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+impl Direction{
+    pub fn opposite(&self)->Direction{     //so that snake ignore the command of going exact opposite direction
+        match *self{
+            Direction::Down=>Direction::Up,
+            Direction::Left=>Direction::Right,
+            Direction::Right=>Direction::Left,
+            Direction::Up=>Direction::Down
+        }
+    }
+}
+
+struct Block{
+    x:i32,
+    y:i32
+}
