dd7130e39e0231f9e545782ab4bfc4b0d2f0c835
shubham <shubham@saasden.club>
2024-03-23 19:17:16 +0000

    feat: helper functions made

---
+++ src/snake.rs
@@ -0,0 +1,7 @@
+use std::collections::LinkedList;
+use piston_window::{Context,G2d};
+use piston_window::types::Color;
+
+use crate::draw::draw_block;
+
+const SNAKE_COLOR:Color=[0.00,0.80,0.00,1.0]
